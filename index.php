044e76a25c824650ea3f801b1750d1cf9c64890a
Honza-B <benedikj.jan@gmail.com>
2013-10-17 04:23:34 +0200

    fifth

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<div id="fb-root"></div>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/cs_CZ/all.js#xfbml=1&appId=330927440385257";
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+        (document, 'script', 'facebook-jssdk')
+    );
+</script>
+
 <?php
 include('src/facebook.php');
 
@@ -13,8 +26,8 @@ $facebook = new Facebook(array(
 
 $signedRequest = $facebook->getSignedRequest();
 if($signedRequest['page']['liked'] == 1) {
-    echo "LIKED";
+    echo "Stránka se vám líbí";
 } else {
-    echo "NOT LIKED";
+    echo '<div class="fb-like-box" data-href="https://www.facebook.com/pages/Med-nen%C3%AD-jed/649741231722878" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>';
 }
 ?>
