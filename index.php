77cc46e780116ac8d432cc3e6ba2459350eb5251
Honza-B <benedikj.jan@gmail.com>
2013-10-17 04:14:25 +0200

    fourth

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-  echo "ahoj";
+include('src/facebook.php');
+
+$config['appid' ] = "154817238062054";
+$config['secret'] = "651640527c71851130c2e60c5bcbd138";
+
+// Create our Application instance.
+$facebook = new Facebook(array(
+    'appId'  => $config['appid'],
+    'secret' => $config['secret'],
+    'cookie' => true
+));
+
+$signedRequest = $facebook->getSignedRequest();
+if($signedRequest['page']['liked'] == 1) {
+    echo "LIKED";
+} else {
+    echo "NOT LIKED";
+}
 ?>
